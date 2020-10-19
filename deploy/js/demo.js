3d3d7b82e7c1352d837f17cd2902f5d997f7d9bf
emheyy45 <63681829+emheyy45@users.noreply.github.com>
2020-10-19 11:29:21 -0400

    First Push
    Est. SVG hourglass

---
+++ deploy/js/demo.js
@@ -0,0 +1,12 @@
+//import {gsap} from "gsap";
+
+// what to animate? / How long?
+//var mSpeed = 2;
+//var tSpeed = 2;
+//var textSpeed = 2;
+
+//gsap.to(".ball-m",{duration: mSpeed, x: 300, rotation: 90, y:150});
+
+//gsap.to(".square-t", {duration: tSpeed, x: 200, rotation: 90, y: -150});
+
+//gsap.to("#text",{duration: textSpeed, x: 250, y: 150});
