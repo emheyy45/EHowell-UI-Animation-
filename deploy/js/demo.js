eea21581d938cc55ef2725c4590b883ffe463273
emheyy45 <63681829+emheyy45@users.noreply.github.com>
2020-10-20 16:11:16 -0400

    push push

--- deploy/js/demo.js
+++ deploy/js/demo.js
@@ -1,9 +1,9 @@
 import {gsap} from "gsap";
 
 const moveBallTL = gsap.timeline();
-moveBallTL.from("#circle-1", {duration:1, y: -200, ease:"bounce"})
-            .to ("#circle-2",{duration:1, y:-200, ease:"bounce"})
-            .to ("#circle-3",{duration:1, y:-200, ease:"bouce"})
+moveBallTL.from("#circle-1", {duration:1, y: -200, easeOut:"bounce"})
+            .to ("#circle-2",{duration:1, y:-200, easeOut:"bounce"})
+            .to ("#circle-3",{duration:1, y:-200, easeOut:"bouce"})
 
 export function moveBallAnimation(){
    return moveBallTL; 
