991119ac7a24028415a8fd77e9b2c36369bc7948
emheyy45 <63681829+emheyy45@users.noreply.github.com>
2020-10-21 00:09:07 -0400

    push again wooo

--- deploy/js/demo.js
+++ deploy/js/demo.js
@@ -1,10 +1,18 @@
 import {gsap} from "gsap";
 
-const moveBallTL = gsap.timeline();
-moveBallTL.from("#circle-1", {duration:1, y: -200, easeOut:"bounce"})
-            .to ("#circle-2",{duration:1, y:-200, easeOut:"bounce"})
-            .to ("#circle-3",{duration:1, y:-200, easeOut:"bouce"})
+gsap.set("#bouncie-mask",{transformOrigin:"center"});
 
+const moveBallTL = gsap.timeline({repeat: -1});
+moveBallTL.from("#svg-image", {duration:1, y: -100, ease:"bounce"})
+            .from ("rectangles", {duration:.5, rotate: 200})
+            .to ("#svg-image", {duration:1, y:-100, ease:"bounce"})
+            .to ("#svg-image", {duration:1, x:150, ease:"bounce"})
+            .to ("#svg-image", {duration:1, y:100, ease:"bounce"})
+            .to ("#svg-image", {duration:1, x:400, ease:"bounce"})
+            
+            
+            
 export function moveBallAnimation(){
-   return moveBallTL; 
+   
+
 }
--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -5153,13 +5153,21 @@
       // to protect from tree shaking
   TweenMaxWithCSS = gsapWithCSS.core.Tween;
 
-  const moveBallTL = gsapWithCSS.timeline();
-  moveBallTL.from("#circle-1", {duration:1, y: -200, easeOut:"bounce"})
-              .to ("#circle-2",{duration:1, y:-200, easeOut:"bounce"})
-              .to ("#circle-3",{duration:1, y:-200, easeOut:"bouce"});
+  gsapWithCSS.set("#bouncie-mask",{transformOrigin:"center"});
 
+  const moveBallTL = gsapWithCSS.timeline({repeat: -1});
+  moveBallTL.from("#svg-image", {duration:1, y: -100, ease:"bounce"})
+              .from ("rectangles", {duration:.5, rotate: 200})
+              .to ("#svg-image", {duration:1, y:-100, ease:"bounce"})
+              .to ("#svg-image", {duration:1, x:150, ease:"bounce"})
+              .to ("#svg-image", {duration:1, y:100, ease:"bounce"})
+              .to ("#svg-image", {duration:1, x:400, ease:"bounce"});
+              
+              
+              
   function moveBallAnimation(){
-     return moveBallTL; 
+     
+
   }
 
   const mainTL = gsapWithCSS.timeline();
