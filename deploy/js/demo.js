d83a8e5c85fb400726af51943f3f3097e1131345
emheyy45 <63681829+emheyy45@users.noreply.github.com>
2020-10-21 09:34:40 -0400

    push

--- deploy/js/demo.js
+++ deploy/js/demo.js
@@ -3,12 +3,11 @@ import {gsap} from "gsap";
 gsap.set("#bouncie-mask",{transformOrigin:"center"});
 
 const moveBallTL = gsap.timeline({repeat: -1});
-moveBallTL.from("#svg-image", {duration:1, y: -100, ease:"bounce"})
-            .from ("rectangles", {duration:.5, rotate: 200})
-            .to ("#svg-image", {duration:1, y:-100, ease:"bounce"})
+moveBallTL.from ("#svg-image", {duration:1, y:-100, ease:"bounce"})
             .to ("#svg-image", {duration:1, x:150, ease:"bounce"})
             .to ("#svg-image", {duration:1, y:100, ease:"bounce"})
             .to ("#svg-image", {duration:1, x:400, ease:"bounce"})
+            
             
             
             
--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -5156,12 +5156,11 @@
   gsapWithCSS.set("#bouncie-mask",{transformOrigin:"center"});
 
   const moveBallTL = gsapWithCSS.timeline({repeat: -1});
-  moveBallTL.from("#svg-image", {duration:1, y: -100, ease:"bounce"})
-              .from ("rectangles", {duration:.5, rotate: 200})
-              .to ("#svg-image", {duration:1, y:-100, ease:"bounce"})
+  moveBallTL.from ("#svg-image", {duration:1, y:-100, ease:"bounce"})
               .to ("#svg-image", {duration:1, x:150, ease:"bounce"})
               .to ("#svg-image", {duration:1, y:100, ease:"bounce"})
               .to ("#svg-image", {duration:1, x:400, ease:"bounce"});
+              
               
               
               
